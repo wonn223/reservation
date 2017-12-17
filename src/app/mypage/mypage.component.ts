cee3ee1d221933dc1f7da73e9a3a68458d1729ec
wj-soft <wooju_lee@daum.net>
2017-12-17 21:45:09 +0900

    reservation list add

--- src/app/mypage/mypage.component.ts
+++ src/app/mypage/mypage.component.ts
@@ -1,4 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { HttpHandler } from '@angular/common/http/src/backend';
+
+
+interface ReservationLists {
+  shopName: string
+  party: number
+  resDate: string
+  resTime: string
+  shopAddress: string
+  shopTel: string
+ 
+}
 
 @Component({
   selector: 'app-mypage',
@@ -6,11 +20,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mypage.component.css']
 })
 export class MypageComponent implements OnInit {
+  appUrl = environment.apiUrl;
+  resList: any
 
+  // 회원의 예약리스트가져오기
+  myReservation(){
+    const headers = {
+      // 'WWW-Authenticate' : 'Token',
+      'Authorization': 'Token be0c1c5b0929bb2937e9976e73524ab45d51609d'
+    }
+    const options = {
+      headers: new HttpHeaders(headers)
+    }
+    this.http.get<ReservationLists[]>(`${this.appUrl}/reservations/customer/`, options)
+      .subscribe(reservationList => {
+        console.log(reservationList)
+        this.resList = reservationList.map(list => Object.assign({},
+          {
+            shopName: list.restaurant.name,
+            party: list.party,
+            resDate: list.information.date,
+            resTime: list.information.time,
+            shopAddress: list.restaurant.address
+          }))
+          console.log(this.resList)
+      })  
+  }
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
 
   ngOnInit() {
+
+    this.myReservation()
   }
 
 }
