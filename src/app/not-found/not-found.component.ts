5a1d74bcc3b9ef951b9b06d35cddf6ef29f34a61
wj-soft <wooju_lee@daum.net>
2017-12-19 20:36:30 +0900

    not found page basic

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -3,11 +3,43 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-not-found',
   template: `
-    <p>
-      페이지가 존재하지 않습니다
-    </p>
-  `,
-  styles: []
+   <div>
+  <h1>404</h1>
+  <h2>Page Not Found</h2>
+
+  <p>Sorry, I couldn't find the page you were looking for.</p>
+
+  <p>Try going <a href="#">back to where you came from</a>, <a href="../">up a directory</a>, or to the <a href="/">home page</a>.</p>
+</div>`,
+  styles: [`
+  div {
+  background: lightgray;
+  padding: 20px;  
+  height: 100vh;
+  text-align: center;
+  box-shadow: 0 0 30px rgba(0, 0, 0, 0.2);
+	text-shadow: 0 2px 1px hsla(0,0%,100%,.2);
+}
+
+h1 {
+	font-size: 2.5em;
+	margin-bottom: 0.2em;
+}
+
+p {
+  	padding-bottom: 3px;
+}
+
+a {
+	text-decoration: none;
+	border-bottom: 1px solid currentColor;
+}
+
+a:hover {
+  color: indigo;
+}
+
+  `]
 })
 export class NotFoundComponent implements OnInit {
 
