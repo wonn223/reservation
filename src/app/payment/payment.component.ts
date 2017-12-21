57ed37d1664aa45a61c1aaf25dee6d931fc95713
wj-soft <wooju_lee@daum.net>
2017-12-21 15:00:00 +0900

    pay module by angular

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -7,6 +7,9 @@ declare var jquery: any;
 declare var $: any;
 declare var IMP: any;
 
+class PayInfo {
+  constructor(public price: number, public uid: string) {};
+}
 
 @Component({
   selector: 'app-payment',
@@ -15,6 +18,7 @@ declare var IMP: any;
 })
 export class PaymentComponent implements OnInit {
 
+  payInfo: PayInfo
   appUrl = environment.apiUrl;
 
   amount = this.shopListService.resInfo.people * this.shopListService.resInfo.price;
@@ -23,6 +27,15 @@ export class PaymentComponent implements OnInit {
   mail: string; // 예약자 이메일
   reservationPk: number;
   imp_uid: string;
+
+  //서버로 전달하기
+  constructor(public shopListService: ShopListService, public http: HttpClient) {
+    this.payInfo = new PayInfo(0, '');
+  }
+
+  ngOnInit() {
+    console.log(this.shopListService.resInfo)
+  }
 
   // 예약관련 함수와 결제.
 
@@ -67,12 +80,38 @@ export class PaymentComponent implements OnInit {
       .subscribe(res => console.log(res))
   }
 
-
+  test(){
+    var req = new XMLHttpRequest();
+    req.open('POST', `http://api.booki.kr/reservations/24/payment`);
+    req.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+    var data = {
+      imp_uid: "imp_080504610870",
+      price: 30000
+    }
+    var dataJ = JSON.stringify(data)
+    console.log(data)
+    console.log(dataJ)
+    req.send(dataJ);
+    // req.onreadystatechange = function (e) {
+    //   if (req.readyState === XMLHttpRequest.DONE) {
+    //     if (req.status === 201) {
+          
+    //       console.log("s")
+    //     } else {
+    //       console.log("Error!");
+    //     }
+    //   }
+    // }
+  }
   // imp UID와 예약정보의 연결방법
 
   // 결제창을 띄우는 함수
   payMode (reservationPk, price) {
     IMP.init('imp56421298'); // 'iamport' 대신 부여받은 "가맹점 식별코드"를 사용
+
+    console.log(this);
+
+    const that = this;
 
     IMP.request_pay({
       pg: "inicis", // version 1.1.0부터 지원.
@@ -84,52 +123,66 @@ export class PaymentComponent implements OnInit {
       buyer_name: this.name,
       buyer_tel: this.tel,
       m_redirect_url: 'http://www.naver.com'
-    }, function (rsp) {
-      if (rsp.success) {
-        var req = new XMLHttpRequest();
-        req.open('POST', `http://api.booki.kr/reservations/${reservationPk}/payment`);
-        req.setRequestHeader('Content-type', 'application/json');
-        var data = {
-          imp_uid: rsp.imp_uid,
-          price: price
-        }
-        // Request를 전송한다
-        req.send(JSON.stringify(data));
-   
-        // angular jquery 에러
-        // jquery.ajax({
-        //   url: `${this.appUrl}/reservations/${this.reservationPk}/payment`, //cross-domain error가 발생하지 않도록 동일한 도메인으로 전송
-        //   type: 'POST',
-        //   dataType: 'json',
-        //   data: {
-        //     imp_uid: rsp.imp_uid,
-        //     //기타 필요한 데이터가 있으면 추가 전달
-        //     price: this.amount
-        //   }
-        // })
-
-        var msg = '결제가 완료되었습니다.';
-        msg += data;
-        msg += reservationPk;
-        msg += price
-        console.log(data)
-      } else {
-        var msg = '결제에 실패하였습니다.';
-        msg += '에러내용 : ' + rsp.error_msg;
-      }
-      alert(msg);
+    }, (res) => {
+      // console.log('[THIS!!!!!]', this);
+      // console.log('[RES]', res);
+      // console.log('[HTTP]', this.http);
+      this.cb(res);
       
     });
-    
-  }
+  };
 
+    // function (rsp) {
+      // if (rsp.success) {
 
-  //서버로 전달하기
-  constructor(public shopListService: ShopListService, public http: HttpClient) { }
+        
 
-  ngOnInit() {
-    console.log(this.shopListService.resInfo)
+      //   var req = new XMLHttpRequest();
+      //   req.open('POST', `http://api.booki.kr/reservations/${reservationPk}/payment`);
+      //   req.setRequestHeader('Content-type', 'application/json');
+      //   var data = {
+      //     "imp_uid": rsp.imp_uid,
+      //     "price": price
+      //   }
+      //   var dataJson = JSON.stringify(data)
+      //   // Request를 전송한다  501ㅇㅔ러
+      //   req.send(dataJson);
+   
+      //   // angular jquery 에러
+      //   // jquery.ajax({
+      //   //   url: `${this.appUrl}/reservations/${this.reservationPk}/payment`, //cross-domain error가 발생하지 않도록 동일한 도메인으로 전송
+      //   //   type: 'POST',
+      //   //   dataType: 'json',
+      //   //   data: {
+      //   //     imp_uid: rsp.imp_uid,
+      //   //     //기타 필요한 데이터가 있으면 추가 전달
+      //   //     price: this.amount
+      //   //   }
+      //   // })
 
-  }
+      //   var msg = '결제가 완료되었습니다.';
+      //   msg += data;
+      //   msg += reservationPk;
+      //   msg += price
+      //   console.log(data)
+      // } else {
+      //   var msg = '결제에 실패하였습니다.';
+      //   msg += '에러내용 : ' + rsp.error_msg;
+      // }
+      // alert(msg);
+      
+    // });
 
+    cb (res) {
+      console.log(res);
+      const payload = {
+        imp_uid: res.imp_uid,
+        price: this.amount
+
+      }
+      console.log(payload);
+      this.http.post(`${this.appUrl}/reservations/${this.reservationPk}/payment/`, payload)
+        .subscribe(res => console.log(res))
+  
+    }
 }
