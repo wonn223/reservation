ea2454bfa51dfdfbace5fa3d314d17bb831c4692
wonn223 <jsw227@hanmail.net>
2017-12-22 16:32:30 +0900

    Update payment.component.ts

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -41,7 +41,7 @@ export class PaymentComponent implements OnInit {
   }
 
   makeTokenInfo() {
-    this.tokenInfo = this.auth.getToken()
+    this.tokenInfo = this.auth.getUserPk()
   }
 
   // 예약관련 함수와 결제.
