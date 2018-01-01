14f7e3206dbbcce680154004475e487cba87668a
wonn223 <jsw227@hanmail.net>
2018-01-01 15:31:04 +0900

    login comp: FB login(not completed)

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,9 +8,12 @@ import { AuthService } from '../services/auth.service';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import { Observable } from 'rxjs/Observable';
+import { Window } from 'selenium-webdriver';
+import { WindowRef } from '@agm/core/utils/browser-globals';
 
-declare const window: any;
-declare const FB: any;
+// declare : 타입스크립트가 아닌 파일의 객체에 any타입 부여
+declare let window: any;
+// declare let FB: any;
 
 @Component({
   selector: 'app-login',
@@ -25,32 +28,32 @@ modalRef: BsModalRef;
 templateRef: TemplateRef<any>;
 
 @Output() open: EventEmitter<Source> = new EventEmitter();
- 
+
   constructor(
     private auth: AuthService,
     private router: Router,
     public modalService: BsModalService,
-    private zone: NgZone
     ) {
-      (function(d, s, id) {
-        let js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) { return; }
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/ko_KR/sdk.js#xfbml=1&version=v2.11';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-      // this.modalRef = this.modalService.show(this.templateRef);
 
-      window.fbasyncInit = () => {
-        console.log('check');
-        FB.init({
-          appId            : 'facebook app id here',
-          autoLogAppEvents : true,
-          xfbml            : true,
-          version          : 'v2.10'
-        });
-        FB.AppEvents.logPageView();
-      };
+      (function(d, s, id) {
+        let js: HTMLScriptElement;
+        let _js = d.getElementsByTagName(s)[0];
+        const fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) { return; }
+        // document객체에 script 엘리먼트를 만든다.
+        _js = d.createElement(s);
+        // double assertion
+        // Element타입 => HTMLScriptElement타입
+        js = (<HTMLScriptElement> <any>_js);
+        // <script id=""></script>
+        js.id = id;
+        js.src = `https://connect.facebook.net/ko_KR/sdk.js#xfbml=1&version=v2.11`;
+        fjs.parentNode.insertBefore(js, fjs);
+        console.log('자바스크립트 링크 완료');
+        console.log(js);
+        console.log(js.src);
+      }(document, 'script', 'facebook-jssdk'));
+
     }
 
 
@@ -59,7 +62,7 @@ templateRef: TemplateRef<any>;
     this.auth.signin(this.loginForm.value)
       .subscribe(
       () => {
-        this.open.emit({bool: true, token: this.auth.token} as Source);
+        this.open.emit({bool: true, token: this.auth.token});
         this.modalRef.hide();
         this.router.navigate(['step']);
       },
@@ -76,7 +79,7 @@ templateRef: TemplateRef<any>;
     this.auth.signout()
    .subscribe(
      () => {
-       this.open.emit(false);
+       this.open.emit({bool: false, token: this.auth.token});
        alert('방문해주셔서 감사합니다');
        this.router.navigate(['main']);
      },
@@ -92,6 +95,7 @@ templateRef: TemplateRef<any>;
   ngOnInit() {
     this.templateRef = this.auth.templateRef;
     this.modalRef = this.auth.headerModalRef;
+
     this.loginForm = new FormGroup({
       email: new FormControl('', [
         Validators.required,
@@ -99,17 +103,35 @@ templateRef: TemplateRef<any>;
       ]),
       password: new FormControl('', [ Validators.required, Validators.minLength(4)])
      });
-    console.log(this.loginForm);
+ 
+     console.log(window.fb);
+
+     window.fbasyncInit = () => {
+      console.log('check');
+      window.FB.init({
+        appId            : '232105043996115',
+        autoLogAppEvents : true,
+        xfbml            : true,
+        version          : 'v2.11'
+      });
+
+    window.FB.AppEvents.logPageView();
+    console.log('check2');
+    };
+
+    if (window.FB) {
+      window.FB.XFBML.parse();
+    }
   }
 
   ngOnChanges() {
     // task 큐에 더 이상 task가 없을 때 알려주는 메소드
-    this.zone.onMicrotaskEmpty
+    // this.zone.onMicrotaskEmpty
     // 루트 존인 NgZone을 구독
-      .subscribe(() => {
+      // .subscribe(() => {
         // 앵귤러 바깥에서 처리된 태스크를 다시 앵귤러 존으로 진입시키는 역할
-        this.zone.run( () => console.log('done'));
-    });
+        // this.zone.run( () => console.log('done'));
+    // });
   }
 
   get email() {
